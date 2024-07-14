8128cdabad4c28543bb25150ec11f5f635462bb2
goosebbeoms <seo980916@gmail.com>
2024-07-14 17:33:58 +0900

    255. State(상태)로 매 키보드 입력마다 유효성 검사하기

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,6 +8,8 @@ export default function Login() {
     password: '',
   });
 
+  const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log('Submitted!');
@@ -28,6 +30,7 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" onChange={(event) => handleInputChange('email', event.target.value)} value={enteredValues.email} />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
